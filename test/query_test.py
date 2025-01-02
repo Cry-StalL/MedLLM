d540782af552e8d1d86ad95f06e6937b9c2e3d86
Cortantse <xuzhj33@mail2.sysu.edu.cn>
2025-01-03 01:00:17 +0800

    新增sample的方法，包括思维链，自平衡，提示词工程，因环境问题还未测试

--- test/query_test.py
+++ test/query_test.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     config = ConfigLoader().get_config()
 
     model_instance = LLMHandler(config['model']['name_or_path'])
-    response = model_instance.generate_response("你是谁?")
+    response = model_instance.generate_response_in_a_letter("你是谁?")
     print(response)
